326769f634ca743acdacf7178c71d5113dfcba97
zowbman <zhangweibao@kugou.net>
2016-09-09 10:03:24 +0800

    变更：1、ads项目模板文件service实现类

--- src/main/java/com/boboface/ads/service/impl/AdsContentServiceImpl.java
+++ src/main/java/com/boboface/ads/service/impl/AdsContentServiceImpl.java
@@ -2,6 +2,8 @@ package com.boboface.ads.service.impl;
 
 import java.util.List;
 
+import net.zowbman.base.model.vo.EnvEnum;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 
 import com.boboface.ads.model.po.TBobofaceAdsContent;
+import com.boboface.ads.model.vo.AdsProjectBuildContent;
 import com.boboface.ads.service.IAdsContentService;
 import com.boboface.base.service.impl.BaseServiceImpl;
 
@@ -34,5 +37,31 @@ public class AdsContentServiceImpl extends BaseServiceImpl<TBobofaceAdsContent> 
 		Example example = new Example(TBobofaceAdsContent.class);
 		example.createCriteria().andEqualTo("appid", appId);
 		return tBobofaceAdsContentMapper.selectByExample(example);
+	}
+
+	@Override
+	public AdsProjectBuildContent getByAppIdAndEnv(Integer appId, EnvEnum envEnum) {
+		Example example = new Example(TBobofaceAdsContent.class);
+		TBobofaceAdsContent mappingFile = new TBobofaceAdsContent();//映射内容
+		TBobofaceAdsContent templateFile = new TBobofaceAdsContent();//模板内容
+		
+		String title = "default.map.online";//默认线上模板
+		if(EnvEnum.dev.equals(envEnum)){
+			title = "default.map.dev";
+		}else if(EnvEnum.test.equals(envEnum)){
+			title = "default.map.test";
+		}else if(EnvEnum.pre.equals(envEnum)){
+			title = "default.map.pre";
+		}
+		
+		example.createCriteria().andEqualTo("title", "tpl.setting");
+		mappingFile = tBobofaceAdsContentMapper.selectByExample(example).iterator().next();
+		example = new Example(TBobofaceAdsContent.class);
+		example.createCriteria().andEqualTo("title", title);
+		example.createCriteria().andEqualTo("appid", appId);
+		templateFile = tBobofaceAdsContentMapper.selectByExample(example).iterator().next();
+		
+		AdsProjectBuildContent adsProjectBuildContent = new AdsProjectBuildContent(appId,mappingFile.getContent(),templateFile.getContent(),envEnum);
+		return adsProjectBuildContent;
 	}
 }
